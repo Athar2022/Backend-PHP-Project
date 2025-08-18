2b6a5dec35c7f5bcca42953d38105e8a9472bf85
Athar2022 <athar.hamady@gmail.com>
2025-08-18 22:59:03 +0300

    Connect to the database

---
+++ routes/web.php
@@ -0,0 +1,17 @@
+<?php
+
+
+use App\Core\Router;
+
+
+$router=new Router();
+
+// $router->get('/blog-mvc/public/posts',[PostController::class,'index']);
+
+// $router->get('/blog-mvc/public/posts/create',[PostController::class,'create']);
+
+// $router->post('/blog-mvc/public/posts',[PostController::class,'store']);
+
+// $router->get('/blog-mvc/public/users' ,handler: [UserController::class ,'index']);
+
+// $router->get('/blog-mvc/public/users/create' ,handler: [UserController::class ,'create']);
